b913150a861f2b0e2bc871e5c91ea05c6a3b2173
DaeSoo <qkajek@gmail.com>
2024-01-06 22:08:50 +0900

    [FEAT] User Entity

---
+++ src/main/java/com/daesoo/sso/member/entity/User.java
@@ -0,0 +1,58 @@
+package com.daesoo.sso.member.entity;
+
+import com.daesoo.sso.member.entity.enums.ProviderType;
+import com.daesoo.sso.member.entity.enums.Role;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Table(name = "users")
+@NoArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    /* 회원 이름 */
+    @Column(nullable = false)
+    private String name;
+
+    /* email(login id) */
+    @Column(nullable = false)
+    private String email;
+
+    /* 비밀번호 */
+    @Column(nullable = false)
+    private String passwd;
+
+    /* 권한 */
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Role role;
+
+    /* 공급자 */
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private ProviderType providerType;
+
+
+    @Builder
+    public User(String name, String email, String passwd, Role role, ProviderType providerType){
+        this.name = name;
+        this.email = email;
+        this.passwd = passwd;
+        this.role  = role;
+        this.providerType = providerType;
+    }
+}
